8dfb7261d2b770275f5b3f613281abe25d1a1dca
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-03-27 16:16:30 +0530

    adding

--- Walmart_ecommerce/app.js
+++ Walmart_ecommerce/app.js
@@ -2,6 +2,14 @@ const exp = require('express');
 const app = exp();
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
+const rp = require('request-promise');
+var myResponse = require('./data');
+
+app.use(exp.static('./public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req,res) => {
     res.sendFile('index.html', {
@@ -10,10 +18,22 @@ app.get('/', (req,res) => {
 });
 
 app.post('/api/search',(req,res)=>{
-    console.log(req.body);
-    res.json({
-        data:[],
+    let walmart = 'http://api.walmartlabs.com/v1/search?apiKey=yjrruzc9fgbxvs28qw2rjczv&format=json&query='+req.body.item;
+    console.log(walmart);
+    
+    rp(walmart)
+    .then(function (htmlString) {
+        // Process html...
+        console.log(res.json(JSON.parse(htmlString)));
+    })
+    .catch(function (err) {
+        // Crawling failed...
     });
+
+    // res.json(
+    //    // data:[],
+    //    myResponse,
+    // );
 })
 
 app.listen(3000);
--- Walmart_ecommerce/public/click.js
+++ Walmart_ecommerce/public/click.js
@@ -1,12 +1,53 @@
+
 $('#search').bind('click', function () {
     let searched_value = $('#find_items').val();
     $.ajax({
         url: '/api/search',
         type: 'POST',
-        data: searched_value
+        data: {item:searched_value}
     }).then(result => {
-        console.log(result);
+        console.log(result.response);
+        $('.row').html('');
+        result.items.forEach((element,index) => {
+            let short = element.shortDescription.substr(0,20);
+            let card = `<div class="col-md-4  ">
+            <div class="card text-white bg-secondary mb-3" style="max-width: 20rem;">
+                <div class="card-header">
+                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModal">${element.name}</button>
+                </div>
+                <div class="card-body">
+                    <img src=${element.thumbnailImage} alt="Smiley face" height="200" width="200">
+                    <p id="less"> ${short}...</p>
+                    <button type='button' class="btn btn-warning" data-toggle="modal" data-target="#exampleModal${index}">Show details</button>
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" id="exampleModal${index}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${element.name}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ${element.shortDescription};
+                </div>
+
+            </div>
+        </div>
+    </div>`;
+
+       // $('body').append(info);
+       $('.row').append(card);
+        });
+        
     }).catch(err => {
         console.log(err);
     })
+
+
 });
